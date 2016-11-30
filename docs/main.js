207cf73ca5396792e4ef3edb2e3583a784d8330c
turbographics2000 <turbographics2000@yahoo.co.jp>
2016-12-01 04:53:45 +0900

    modify

--- docs/main.js
+++ docs/main.js
@@ -36,7 +36,7 @@ btnFromURL.onclick = function () {
                 .then(blob => createStreamTrack(blob, 'video'))
         }).then(track => {
             tracks = tracks.concat(track);
-            preview.srcObject = new MediaStream([tracks[0]]);
+            preview.srcObject = new MediaStream([tracks]);
         });
 }
 
